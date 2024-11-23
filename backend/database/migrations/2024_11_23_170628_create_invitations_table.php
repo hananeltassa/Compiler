40dde682d6976a9ed4e4c5ad9604b03e070f735d
hananeltassa <hananeltassa15@gmail.com>
2024-11-23 19:07:50 +0200

    Migrated the db

---
+++ backend/database/migrations/2024_11_23_170628_create_invitations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->string('invited_email');
+            $table->foreignId('file_id')->constrained('files')->onDelete('cascade'); 
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
+            $table->enum('status', ['pending', 'accepted', 'declined'])->default('pending'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
