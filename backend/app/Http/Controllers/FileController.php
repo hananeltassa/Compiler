740d4711b148ebd2127b3918fb994762cead6a56
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 22:27:38 +0200

    Saved file in public storage

--- backend/app/Http/Controllers/FileController.php
+++ backend/app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
@@ -13,5 +14,14 @@ class FileController extends Controller
             'content' => 'nullable|string',   
             'user_id' => 'required|exists:users,id',
         ]);
+
+        // generating a unique file path using uniqid()
+        $path = 'files/' . uniqid() . '_' . $validated['name'];
+
+        // saving in public storage, if empty save empty file
+        Storage::disk('public')
+        ->put($path, !empty($validated['content']) ? $validated['content'] : '');
+
+
     }
 }
