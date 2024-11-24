c4e4c4cc406852c3779d2507111aa59d8d594c82
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-24 12:14:37 +0200

    implemented the jsx needed for the login page -frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import styles from "../styles/auth.module.css";
+import useForm from "../hooks/useForm";
 
 const Login = () => {
-
+    const navigate = useNavigate();
+    const {form, updateForm} = useForm({
+        email: "",
+        password: "",
+    });
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
-        <h1>hi</h1>
+        <div className={styles.signupContainer}>
+            <form className={styles.signupForm}>
+                <h1>Sign In</h1>
+                Email
+                <input type="email" placeholder="Email" name="email" onChange={updateForm} required />
+                Password
+                <input type="password" placeholder="Password" name="password" onChange={updateForm} required />
+                <button type="submit" onClick={handleSubmit}>
+                    Sign In
+                </button>
+                <p>
+                    Do not have an account?
+                    <span className={styles.loginLink} onClick={() => navigate("/")}>
+                        Sign-up
+                    </span>
+                </p>
+            </form>
+        </div>
     );
-}
-
+};
 export default Login;
