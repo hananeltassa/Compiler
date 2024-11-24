3460f7adc030a8e4bae9abbfcc160678494a46c3
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-24 15:08:09 +0200

    linked the login api with the login page -frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,9 +8,19 @@ const Login = () => {
         email: "",
         password: "",
     });
-    function handleSubmit(e) {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-    }
+        const response = await fetch("http://127.0.0.1:8000/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        });
+
+        const data = await response.json();
+        console.log(data);
+    };
     return (
         <div className={styles.signupContainer}>
             <form className={styles.signupForm}>
