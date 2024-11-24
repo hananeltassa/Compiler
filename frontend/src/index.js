80396535e479b2ebb6353a9c75a8c7ffd953f339
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-24 18:50:44 +0200

    removed the chakra related stuff -frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {ChakraProvider} from "@chakra-ui/react";
-import theme from "../components/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <App />
-        </ChakraProvider>
+        <App />
     </React.StrictMode>
 );
--- frontend/src/pages/Compiler.jsx
+++ frontend/src/pages/Compiler.jsx
@@ -1,6 +1,4 @@
-import {Box} from "@chakra-ui/react";
-
 const Compiler = () => {
-    return <Box>Hello</Box>;
+    return <div>Hello</div>;
 };
 export default Compiler;
