e6e8f12c61c46de32b1e6a9d9b2849fa7c3dcd01
hananeltassa <hananeltassa15@gmail.com>
2024-11-27 14:13:52 +0200

    Updated open ai - backend

--- backend/app/Http/Controllers/CodeAnalysisController.php
+++ backend/app/Http/Controllers/CodeAnalysisController.php
@@ -13,7 +13,8 @@ class CodeAnalysisController extends Controller
             'code' => 'required|string',
         ]);
 
-        $openai = \OpenAI::client('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');
+        $apiKey = env('OPENAI_API_KEY');
+        $openai = \OpenAI::client($apiKey);
 
         $messages = [
             ['role' => 'system', 'content' => 'You are a helpful assistant.'],
